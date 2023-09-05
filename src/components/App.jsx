d5e083c61ef47828405ed587632090358ca6660f
Łukasz Paradowski <123119119+Lukaszz2003@users.noreply.github.com>
2023-09-05 22:13:02 +0200

    one

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useStat } from 'react';
 import Section from './Section/Section';
 import Statistics from './Statistics/Statistics';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
